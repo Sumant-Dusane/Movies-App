3c15f07bc98a727cc604f3f6442070e374a3ac45
Sumant-Testbook <sumant.dusane@testbook.com>
2023-04-22 11:02:31 +0530

    changed apo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -26,7 +26,7 @@ export class HomeComponent{
   }
 
   setData(reponse: any) {
-    this.searchData = reponse.Search;
+    this.searchData = reponse.d;
     this.isSkeleton = false;
     console.log(this.searchData);
   }
--- src/app/services/Apis/network.service.ts
+++ src/app/services/Apis/network.service.ts
@@ -7,14 +7,21 @@ import { map } from 'rxjs';
 })
 export class NetworkService {
 
-  apikey = '39ccf8ac';
-  endpoint = 'https://www.omdbapi.com/?apikey=' + this.apikey;
+  headers = new HttpHeaders({
+    'x-rapidapi-host': 'imdb8.p.rapidapi.com',
+    'x-rapidapi-key': '7c1834b123msh528683e4aab2695p16995fjsn5ec42d00ed6b'
+  });
+
+  // apikey = '39ccf8ac';
+  // endpoint = 'https://www.omdbapi.com/?apikey=' + this.apikey;
+
+  endpoint = 'https://imdb8.p.rapidapi.com/';
 
   constructor(private http: HttpClient) { }
 
   getDatafromSearch(searchParam: string | null) {
-    let url = this.endpoint + '&s=' + searchParam;
-    return this.http.get(url);
+    let url = this.endpoint + 'auto-complete?q=' + searchParam;
+    return this.http.get(url, {headers: this.headers});
   }
 
   // getTrendingMoviesID() {
